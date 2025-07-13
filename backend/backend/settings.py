e17f86fb38e0008bec55f0ea8630eb598d1c644a
Demo-23home <zeyadslama23@gmail.com>
2025-07-13 14:51:27 +0300

    Creating job Model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,64 +11,79 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+import dotenv
 
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+dotenv.read_dotenv()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$q!0np&l83rx+8idmqg+c2$9aj4_^xmw_+d=1mxv@(z7uvhd#7'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
-    "django.contrib.gis"
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+
+LOCAL_APPS = ["job"]
+
+THIRD_PARTY_APPS = [
+    "django.contrib.gis",
+    "rest_framework",
+    "corsheaders",
+    "storages",
+    "django_filters",
+]
+
+
+INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
+
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ], 
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = "backend.wsgi.application"
 
 
 # Database
@@ -83,17 +98,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': "jobee-api", 
-        "USER": "postgres", 
-        "PASSWORD": "toor", 
-        "HOST": "localhost", 
-        "PORT": 5432
+    "default": {
+        "ENGINE": "django.contrib.gis.db.backends.postgis",
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
-
-
 
 
 # Password validation
@@ -101,16 +114,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -118,9 +131,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -130,14 +143,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 GDAL_LIBRARY_PATH = os.environ.get("GDAL_LIBRARY_PATH")
---
+++ backend/job/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.contrib.gis.db import models as gismodels
+from django.contrib.gis.geos import Point
+import datetime
+from django.contrib.auth.models import User
+import geocoder, os
+
+
+class JobTypes(models.TextChoices):
+    Permanent = "Permanent"
+    Temporary = "Temporary"
+    Internship = "Internship"
+
+
+class Education(models.TextChoices):
+    Bachelors = "Bachelors"
+    Masters = "Masters"
+    Phd = "Phd"
+
+
+class Industry(models.TextChoices):
+    Business = "Permanent"
+    IT = "Temporary"
+    Banking = "Internship"
+    Education = "Education"
+    Telecommunication = "Telecommunication"
+    Others = "Others"
+
+
+class Experience(models.TextChoices):
+    NO_EXPERIENCE = "No experience"
+    ONE_YEAR = "1 Year"
+    TO_YEAR = "2 Year"
+    THREE_YEAR_PLUS = "3 Year above"
+
+
+def return_date_time():
+    now = datetime.now()
+    date = now + datetime.timedelta(days=10)
+    return date
+
+
+class Job(models.Model):
+    title = models.CharField(_(""), max_length=50)
+    description = models.TextField(_(""))
+    email = models.EmailField(_(""), max_length=254)
+    address = models.CharField(_(""), max_length=50)
+    job_type = models.CharField(
+        _(""), max_length=50, choices=JobTypes.choices, default=JobTypes.Permanent
+    )
+    education = models.CharField(
+        _(""), max_length=50, choices=Education.choices, default=Education.Bachelors
+    )
+    industry = models.CharField(
+        _(""), max_length=50, choices=Industry.choices, default=Industry.Business
+    )
+    experience = models.CharField(
+        _(""),
+        max_length=50,
+        choices=Experience.choices,
+        default=Experience.NO_EXPERIENCE,
+    )
+    salary = models.PositiveIntegerField(
+        _(""), default=1, validators=[MinValueValidator(1), MaxValueValidator(1000000)]
+    )
+    positions = models.PositiveIntegerField(_(""), default=1)
+    company = models.CharField(_(""), max_length=50, null=True)
+    point = gismodels.PointField(default=Point(0.0, 0.0))
+    last_date = models.DateTimeField(_(""), default=return_date_time)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+    def save(self, *args, **kwargs): 
+        g = geocoder.opencage(self.address, key=os.environ.get("GEOCODER_API"))
+        print(g)
+        lng = g.lng 
+        lat = g.lat 
+        
+        self.point = Point(lng, lat)
+        super(Job, self).save(*args, **kwargs)
