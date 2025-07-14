14ab2d0fa1cd65f32997bd860b1ddfc88ae1591c
Demo-23home <zeyadslama23@gmail.com>
2025-07-14 11:30:15 +0300

    Adding Job's Pagination

--- backend/job/views.py
+++ backend/job/views.py
@@ -4,15 +4,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg, Count, Min, Max
-from.filters import JobFilter
+from .filters import JobFilter
+from rest_framework.pagination import PageNumberPagination
+
 
 @api_view(["GET"])
 def list_jobs(request):
     jobs = Job.objects.all().order_by("id")
     filterset = JobFilter(request.GET, jobs)
 
-    serializer = JobSerializer(filterset.qs, many=True)
-    return Response(serializer.data)
+    count = filterset.qs.count()
+    resPerPage = 3
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+
+    serializer = JobSerializer(queryset, many=True)
+    return Response({"count": count, "resPerPage": resPerPage, "jobs": serializer.data})
 
 
 @api_view(["GET"])
