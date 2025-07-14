9ee45102bb9fa7ab61acc8374c5bc943784348aa
Demo-23home <zeyadslama23@gmail.com>
2025-07-14 15:38:21 +0300

    Adding Login Fucntion

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
     path("signup/", views.sign_up, name="signup"),
+    path("login/", TokenObtainPairView.as_view(), name="signup"),
+    path("refresh/", TokenRefreshView.as_view(), name="signup"),
+    path("verify/", TokenVerifyView.as_view(), name="signup"),
 ]
