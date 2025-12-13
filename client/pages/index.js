8e4ad31a85d94444de15b74e8fc187608c2e9430
Demo-23home <zeyadslama23@gmail.com>
2025-12-13 21:49:41 +0200

    Making Server-Client connection

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,32 @@
 import Layout from "../components/layout/Layout";
 import Home from "../components/Home";
+import axios from "axios";
 
-export default function Index() {
+export default function Index({ data }) {
+  console.log("data", data);
   return (
     <Layout>
       <Home />
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get(`${process.env.API_URL}/jobs/`);
+    const data = res.data;
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching jobs:', error.response?.data || error.message);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
 }
--- config/settings/local.py
+++ config/settings/local.py
@@ -11,11 +11,18 @@ if path.isfile(local_env_file):
 
 DEBUG = True
 
-SECRET_KEY = getenv("DJANGO_SECRET_KEY", "TABl6_TpFit2mi8aNiNv3F23pVdQ9Z09-unNtlfLWNlGavBI5kM")
+SECRET_KEY = getenv(
+    "DJANGO_SECRET_KEY", "TABl6_TpFit2mi8aNiNv3F23pVdQ9Z09-unNtlfLWNlGavBI5kM"
+)
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://client:3000",
+]
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "api"]
 
 DOMAIN = getenv("DOMAIN")
 
